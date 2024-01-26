c50030fd30c8db989b4fbd75f60b4d064afbbf5c
Mateo Streed <mtstreed@gmail.com>
2024-01-26 13:26:00 -0500

    deleted files that should have been gitignored

--- server/dist/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import userController from '../controllers/userController.js';
-const router = express.Router();
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
-export default router;
-//# sourceMappingURL=userRoutes.js.map
